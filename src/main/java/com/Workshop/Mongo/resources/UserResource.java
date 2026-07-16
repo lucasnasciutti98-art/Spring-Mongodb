aa49e3bef47c435e442db0940356f20aaae61a6a
Lucas Nardini <lucasnasciutti98@gmail.com>
2026-07-16 11:47:21 -0300

    Entity user and resource

---
+++ src/main/java/com/Workshop/Mongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.Workshop.Mongo.resources;
+
+import java.net.ResponseCache;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Workshop.Mongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>>findAll(){
+		User maria = new User("1", "Maria Brown", "Maria@gmail.com");
+		User alex = new User("2", "Alex", "Alex@gmail.com");
+		List<User> list= new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
